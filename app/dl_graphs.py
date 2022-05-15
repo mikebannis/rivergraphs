f5574be2a793b83bb602ef2fc2dcbe60ed6773bc
Mike Bannister <mikebannis@gmail.com>
2022-05-14 23:21:06 -0600

    Make DWR hydrographs prettier

--- app/dl_graphs.py
+++ app/dl_graphs.py
@@ -8,6 +8,7 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime as dt
 from datetime import timedelta
+import matplotlib.dates as mdates
 from matplotlib import pyplot as plt
 
 import gageman
@@ -26,10 +27,10 @@ class FailedImageAddr(Exception):
 
 def get_dwr_graph(gage, outfile=None):
     """
-    Grabs default flow graph for Colorado DWR Gage 'gage' 
+    Grabs default flow graph for Colorado DWR Gage 'gage'
 
     param: gage - string ('PLAGRAC' is south platte at grant, i.e. bailey gage)
-    prarm: outfile - name of file to export 
+    prarm: outfile - name of file to export
     """
     # API Doc: https://github.com/OpenCDSS/cdss-rest-services-examples
     gage_url = 'https://dwr.state.co.us/Rest/GET/api/v2/telemetrystations/' +\
@@ -51,7 +52,7 @@ def get_dwr_graph(gage, outfile=None):
     timestamp = last_result['measDateTime']
     date = timestamp.split('T')[0]
     time = timestamp.split('T')[1]
-    
+
     if outfile is None:
         outfile = gage+'.cfs'
 
@@ -61,9 +62,9 @@ def get_dwr_graph(gage, outfile=None):
     # --- Plot and save the hydrograph
     date_f = '%Y-%m-%dT%H:%M:%S'
     raw_qs = [r['measValue'] for r in response.json()['ResultList']]
-    raw_tss = [dt.strptime(r['measDateTime'], date_f) for r in 
+    raw_tss = [dt.strptime(r['measDateTime'], date_f) for r in
                   response.json()['ResultList']]
-    
+
     # only show last 7 days
     qs = []
     tss = []
@@ -74,16 +75,23 @@ def get_dwr_graph(gage, outfile=None):
         qs.append(q)
         tss.append(ts)
 
-    i_outfile = outfile[:-4]+'.png' 
+    i_outfile = outfile[:-4]+'.png'
+
+    fmt = mdates.DateFormatter('%m-%d')
+    plt.gca().xaxis.set_major_formatter(fmt)
+    plt.grid(visible=True)
+    plt.xticks(rotation=45)
+
     plt.plot(tss, qs)
     plt.savefig(i_outfile)
+    plt.close()
 
 
 def get_usgs_gage(gage, outfile=None):
     """
     Grabs default flow graph for USGS Gage 'gage' and write image to outfile.
     param: gage - string ('0671950' is clear creek at golden)
-    prarm: outfile - name of file to export 
+    prarm: outfile - name of file to export
     """
     gage_url = 'https://waterdata.usgs.gov/usa/nwis/uv?'+gage
     if outfile is None:
@@ -94,7 +102,7 @@ def get_usgs_gage(gage, outfile=None):
     # Verify we got good stuff back
     if response.status_code != 200:
         raise URLError(response.status_code, response.text)
-    
+
 #    # Grab the image url
 #    img_addr = None
 #    soup = BeautifulSoup(response.text, 'html.parser')
@@ -111,9 +119,9 @@ def get_usgs_gage(gage, outfile=None):
 #    response = requests.get(img_addr, stream=True, cookies=response.cookies)
 #    with open(outfile, 'wb') as out:
 #        shutil.copyfileobj(response.raw, out)
-    
+
     soup = BeautifulSoup(response.text, 'html.parser')
-	
+
     # Get the discharge
     for tag in soup.find_all('a'):
         if tag.get('name') == "gifno-99":
@@ -128,7 +136,7 @@ def get_usgs_gage(gage, outfile=None):
                 response = requests.get(img_addr, stream=True, cookies=response.cookies)
                 with open(outfile, 'wb') as out:
                     shutil.copyfileobj(response.raw, out)
-                
+
                 # Grab the discharge and save it to a .cfs file
                 _next = parent.next_sibling
                 q = pull_val(_next)
@@ -186,7 +194,7 @@ def main():
             if verbose:
                 print ('success')
 
-        else: 
+        else:
             raise AttributeError('gage was returned from get_gages() that is not USGS or DWR')
         time.sleep(SLEEP)
 
--- app/test_dwr_gage_pull.py
+++ app/test_dwr_gage_pull.py
@@ -1,9 +1,11 @@
-#!/usr/bin/python3  
+#!/usr/bin/env python3
 from dl_graphs import get_dwr_graph
 
 def main():
     bailey = 'PLABAICO'
     get_dwr_graph(bailey)
+    alto = 'BOCPINCO'
+    get_dwr_graph(alto)
 
 
 if __name__ == '__main__':
